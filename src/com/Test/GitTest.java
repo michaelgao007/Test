acc0987cb3da011ab9131c4c301a4941e64be325
Michael <michaelgao007@gmail.com>
2021-11-03 13:30:52 -0400

    ThirdCommit

--- src/com/Test/GitTest.java
+++ src/com/Test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Michael");
+        System.out.println("Hello Git 3");
     }
 }
