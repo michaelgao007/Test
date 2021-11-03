9c362fe48f2defd6162f8fa2239baa4f51d4e2ea
Michael <michaelgao007@gmail.com>
2021-11-03 13:45:41 -0400

    Master 1

--- src/com/Test/GitTest.java
+++ src/com/Test/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello Michael");
         System.out.println("Hello Git 3");
         System.out.println("This is hot fix");
+        System.out.println("This is master!");
     }
 }
