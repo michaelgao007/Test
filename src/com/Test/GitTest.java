7f9f1bda3506c167e1b491ebbad7edb54d0e3489
Michael <michaelgao007@gmail.com>
2021-11-03 13:41:43 -0400

    hot-fix commit 1

--- src/com/Test/GitTest.java
+++ src/com/Test/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello World!");
         System.out.println("Hello Michael");
         System.out.println("Hello Git 3");
+        System.out.println("This is hot fix");
     }
 }
